f2b3d690c905c77fba922063a175ba3db6876903
Gavin Stirling <gavinstir@gmail.com>
2022-10-28 20:15:50 +0100

    Initial commit

---
+++ Models/Recipe.cs
@@ -0,0 +1,18 @@
+﻿namespace MealPlanningAPI.Models
+{
+    public class Recipe
+    {
+        public int RecipeID { get; set; }
+        public string RecipeName { get; set; }
+        public string Difficulty { get; set; }
+        public int PrepTime { get; set; }
+        
+        public Recipe (int recipeID, string recipeName, string difficulty, int prepTime)
+        {
+            RecipeID = recipeID;
+            RecipeName = recipeName;
+            Difficulty = difficulty;
+            PrepTime = prepTime;
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+﻿namespace MealPlanningAPI.Models
+{
+    public class User
+    {
+        public int UserID { get; set; } 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public User (int userID, string firstName, string lastName, string email)
+        {
+            UserID = userID;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+        }
+    }
+}
