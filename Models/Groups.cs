7647067a4a959e8d3445d422ab27432538ab301b
Gavin Stirling <gavinstir@gmail.com>
2022-10-28 20:25:12 +0100

    Set up models

---
+++ Models/Groups.cs
@@ -0,0 +1,29 @@
+﻿namespace MealPlanningAPI.Models
+{
+    public class Groups
+    {
+        public int GroupID { get; set; }
+        public int HouseholdID { get; set; }
+        public int AdminID { get; set; }
+        public string? Members { get; set; }
+        public List<int>? MemberIDs { get; set; }
+
+        public Groups (int groupID, int householdID, int adminID)
+        {
+            GroupID = groupID;
+            HouseholdID = householdID;
+            AdminID = adminID;
+        }
+
+        public Groups(int groupID, int householdID, int adminID, string members) : this(groupID, householdID, adminID)
+        {
+            Members = members;
+            MemberIDs = new List<int>();
+            string[] memberIDs = members.Split(",");
+            foreach(var member in memberIDs)
+            {
+                MemberIDs.Add(Int16.Parse(member));
+            }
+        }
+    }
+}
---
+++ Models/Household.cs
@@ -0,0 +1,14 @@
+﻿namespace MealPlanningAPI.Models
+{
+    public class Household
+    {
+        public int HouseholdId { get; set; }
+        public string HouseholdName { get; set; }
+
+        public Household (int householdId, string householdName)
+        {
+            HouseholdId = householdId;
+            HouseholdName = householdName;
+        }
+    }
+}
